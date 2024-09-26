7da619d8f1e199c995856e3fd8f1c58236d9f4af
Sunyoung Lee <s3u0n4@gmail.com>
2024-09-26 19:20:59 +0900

    svelte,vite로 바꾼 후 firebase 세팅

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import Write from "./pages/Write.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Router from "svelte-spa-router";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Signup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<Router {routes} />
+
+<main></main>
+
+<style>
+</style>
